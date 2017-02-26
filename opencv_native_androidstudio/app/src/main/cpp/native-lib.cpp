66e245f68dbaee37201c5bb6a655a33c27cff1f1
orenafek <oren.afek@gmail.com>
2017-02-26 20:32:07 +0200

    trying to reupload the app

---
+++ opencv_native_androidstudio/app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,90 @@
+#include <jni.h>
+#include <string>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include "openface/LandmarkDetector/include/LandmarkCoreIncludes.h"
+#include <android/log.h>
+#include <iostream>
+#include <fstream>
+#define  LOG_TAG    "ndk-tag"
+#define  LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
+
+using namespace std;
+using namespace cv;
+
+extern "C"
+{
+void JNICALL Java_ch_hepia_iti_opencvnativeandroidstudio_MainActivity_salt(JNIEnv *env, jobject instance,
+                                                                           jlong matAddrGray,
+                                                                           jint nbrElem) {
+    Mat &mGr = *(Mat *) matAddrGray;
+//    for (int k = 0; k < nbrElem; k++) {
+//        int i = rand() % mGr.cols;
+//        int j = rand() % mGr.rows;
+//        mGr.at<uchar>(j, i) = 255;
+//    }
+    vector<string> arguments;
+
+    LandmarkDetector::FaceModelParameters det_parameters;
+    LandmarkDetector::CLNF clnf_model;
+
+    det_parameters.init();
+    det_parameters.model_location = "data/data/ch.hepia.iti.opencvnativeandroidstudio/model/main_clnf_general.txt";
+    det_parameters.face_detector_location = "data/data/ch.hepia.iti.opencvnativeandroidstudio/classifiers/haarcascade_frontalface_alt.xml";
+
+
+    clnf_model.model_location_clnf = "data/data/ch.hepia.iti.opencvnativeandroidstudio/model/main_clnf_general.txt";
+    clnf_model.face_detector_location_clnf = "data/data/ch.hepia.iti.opencvnativeandroidstudio/classifiers/haarcascade_frontalface_alt.xml";
+    clnf_model.inits();
+
+    __android_log_write(ANDROID_LOG_INFO, "JNIDATA",  "Init Successfull!!");
+
+
+    cv::Mat_<float> depth_image;
+    cv::Mat_<uchar> grayscale_image;
+
+
+    grayscale_image = mGr.clone();
+
+
+    // The actual facial landmark detection / tracking
+    bool detection_success = LandmarkDetector::DetectLandmarksInImage(grayscale_image, depth_image, clnf_model, det_parameters);
+
+    if (detection_success) {
+        LandmarkDetector::Draw(mGr, clnf_model);
+    }
+
+    stringstream sstream;
+    sstream << "Value is " << detection_success << endl;
+    //LOGD(sstream.str().c_str());
+    __android_log_write(ANDROID_LOG_INFO, "JNIDATA", sstream.str().c_str());
+
+    // The modules that are being used for tracking
+    //LandmarkDetector::CLNF clnf_model;
+
+    //det_parameters.init();
+    //LandmarkDetector::CLNF clnf_model(det_parameters.model_location);
+
+    //LandmarkDetector::DetectLandmarksInImage(grayscale_image, clnf_model, det_parameters);
+//        ifstream infile();
+//
+//    string line;
+//    ifstream myfile("/data/data/ch.hepia.iti.opencvnativeandroidstudio/model/model_eye/clnf_left_synth.txt");
+//    sstream.str("");
+//    if (myfile.is_open())
+//    {
+//        while ( getline (myfile,line) )
+//        {
+//            sstream << line << '\n';
+//        }
+//        myfile.close();
+//    }
+//
+//    else sstream << "Unable to open file";
+//
+//    __android_log_write(ANDROID_LOG_INFO, "JNIDATA", sstream.str().c_str());
+
+
+}
+}
---
+++ opencv_native_androidstudio/app/src/main/java/ch/hepia/iti/opencvnativeandroidstudio/MainActivity.java
@@ -0,0 +1,208 @@
+package ch.hepia.iti.opencvnativeandroidstudio;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    private static final String TAG = "OCVSample::Activity";
+    private CameraBridgeViewBase _cameraBridgeViewBase;
+
+    private BaseLoaderCallback _baseLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS: {
+                    Log.i(TAG, "OpenCV loaded successfully");
+                    _cameraBridgeViewBase.enableView();
+                }
+                break;
+                default: {
+                    super.onManagerConnected(status);
+                }
+            }
+        }
+    };
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+
+        // Permissions for Android 6+
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE},
+                1);
+
+        String toPath = "/data/data/" + getPackageName();  // Your application path
+        copyAssetFolder(getAssets(),"",toPath);
+
+
+        // Load ndk built module, as specified
+        // in moduleName in build.gradle
+        System.loadLibrary("native-lib");
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.activity_main);
+
+
+
+        _cameraBridgeViewBase = (CameraBridgeViewBase) findViewById(R.id.main_surface);
+        _cameraBridgeViewBase.setVisibility(SurfaceView.VISIBLE);
+        _cameraBridgeViewBase.setCvCameraViewListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        disableCamera();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()) {
+            Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, _baseLoaderCallback);
+        } else {
+            Log.d(TAG, "OpenCV library found inside package. Using it!");
+            _baseLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(MainActivity.this, "Permission denied to read your External storage", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        disableCamera();
+    }
+
+    public void disableCamera() {
+        if (_cameraBridgeViewBase != null)
+            _cameraBridgeViewBase.disableView();
+    }
+
+    public void onCameraViewStarted(int width, int height) {
+    }
+
+    public void onCameraViewStopped() {
+    }
+
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        Mat matGray = inputFrame.gray();
+        salt(matGray.getNativeObjAddr(), 2000);
+        return matGray;
+    }
+
+    public native void salt(long matAddrGray, int nbrElem);
+
+
+
+
+
+
+
+    private static boolean copyAssetFolder(AssetManager assetManager,
+                                           String fromAssetPath, String toPath) {
+        try {
+            String[] files = assetManager.list(fromAssetPath);
+            if (!fromAssetPath.equals("")) {
+                //Not root path
+                fromAssetPath = fromAssetPath + "/";
+            }
+            new File(toPath).mkdirs();
+            boolean res = true;
+            for (String file : files) {
+                if (file.contains(".")) {
+                    Log.d("OPENCV_TAG","Writing" + file);
+                    res &= copyAsset(assetManager,
+                            fromAssetPath + file,
+                            toPath  +"/"+ file);
+                }
+                else {
+                    res &= copyAssetFolder(assetManager,
+                            fromAssetPath + file,
+                            toPath + "/" + file);
+                }
+            }
+            return res;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private static boolean copyAsset(AssetManager assetManager,
+                                     String fromAssetPath, String toPath) {
+        InputStream in = null;
+        OutputStream out = null;
+        try {
+            in = assetManager.open(fromAssetPath);
+            new File(toPath).createNewFile();
+            out = new FileOutputStream(toPath);
+            copyFile(in, out);
+            in.close();
+            in = null;
+            out.flush();
+            out.close();
+            out = null;
+            Log.d("OPENCV_TAG","Yay!");
+            return true;
+        } catch(Exception e) {
+            Log.d("OPENCV_TAG","Bummer :(");
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private static void copyFile(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[1024];
+        int read;
+        while((read = in.read(buffer)) != -1){
+            out.write(buffer, 0, read);
+        }
+    }
+
+}
+
